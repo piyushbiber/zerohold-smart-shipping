2f2ea448d103c6cec3257f9487dd08f4b11bb2ae
Piyushbiber <Piyushbiber@gmail.com>
2026-01-20 18:18:25 +0530

    Debug RateQuote: Log empty object creation to trace data loss

--- Models/RateQuote.php
+++ Models/RateQuote.php
@@ -19,6 +19,9 @@ class RateQuote {
 	public $courier_id;
 
 	public function __construct( $data = [] ) {
+        // ZSS DEBUG: logging constructor input
+        // error_log( "ZSS DEBUG: RateQuote Consturct Input: " . print_r($data, true) );
+        
         if ( is_object( $data ) ) {
             $data = (array) $data;
         }
@@ -32,5 +35,12 @@ class RateQuote {
 		$this->courier_id     = isset( $data['courier_id'] ) ? $data['courier_id'] : ''; // New field
 		$this->vendor_share   = isset( $data['vendor_share'] ) ? $data['vendor_share'] : 0;
 		$this->retailer_share = isset( $data['retailer_share'] ) ? $data['retailer_share'] : 0;
+        
+        // ZSS DEBUG: logging result
+        if ( $this->base > 0 || !empty($this->platform) ) {
+             // error_log( "ZSS DEBUG: RateQuote Created -> Base: {$this->base}, Platform: {$this->platform}" );
+        } else {
+             error_log( "ZSS DEBUG WARNING: RateQuote Created EMPTY -> Input was: " . print_r($data, true) );
+        }
 	}
 }
